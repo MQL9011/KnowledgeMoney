d1b8c8709d349fe2a3ed033d0fa66a4872fbd4fb
马千里 <maqianli@ysten.com>
2018-01-12 20:09:54 +0800

    加上TopSup中的词频方法

--- do.py
+++ do.py
@@ -5,67 +5,16 @@
 import time
 import json
 import requests
-from googleapiclient.discovery import build
 import webbrowser
 from urllib import parse
 import urllib.parse
+from threading import Thread
+import methods
 
 g_cse_id = ''
 g_cse_api_key = ''
 
 questions = []
-
-
-def google_search(query, start):
-    service = build("customsearch", "v1", developerKey=g_cse_api_key)
-    res = service.cse().list(q=query, cx=g_cse_id, start=start).execute()
-    return res
-
-# Google Question and count number of each result
-def metric1Func(question, answers):
-    met1 = [0, 0, 0]
-    res = google_search(question, None)
-    items = str(res['items']).lower()
-    met1[0] = items.count(answers[0].lower())
-    met1[1] = items.count(answers[1].lower())
-    met1[2] = items.count(answers[2].lower())
-    return met1
-
-
-# Google Question and each specific Answer and count total results
-def metric2Func(question, answers):
-    met2 = [0, 0, 0]
-    res0 = google_search(question + ' "' + answers[0] + '"', None)
-    res1 = google_search(question + ' "' + answers[1] + '"', None)
-    res2 = google_search(question + ' "' + answers[2] + '"', None)
-    return [int(res0['searchInformation']['totalResults']), int(res1['searchInformation']['totalResults']), int(res2['searchInformation']['totalResults'])]
-
-
-def predict(metric1, metric2, answers):
-    max1 = metric1[0]
-    max2 = metric2[0]
-    for x in range(1, 3):
-        if metric1[x] > max1:
-            max1 = metric1[x]
-        if metric2[x] > max2:
-            max2 = metric2[x]
-    if metric1.count(0) == 3:
-        return answers[metric2.index(max2)]
-    elif metric1.count(max1) == 1:
-        if metric1.index(max1) == metric2.index(max2):
-            return answers[metric1.index(max1)]
-        else:
-            percent1 = max1 / sum(metric1)
-            percent2 = max2 / sum(metric2)
-            if percent1 >= percent2:
-                return answers[metric1.index(max1)]
-            else:
-                return answers[metric2.index(max2)]
-    elif metric1.count(max1) == 3:
-        return answers[metric2.index(max2)]
-    else:
-        return answers[metric2.index(max2)]
-
 
 def get_answer():
     resp = requests.get('http://htpmsg.jiecaojingxuan.com/msg/current',timeout=4).text
@@ -81,37 +30,35 @@ def get_answer():
             questions.append(question)
             answers = eval(resp_dict['data']['event']['options'])
             search_wd = question + answers[0] + answers[1] + answers[2]
-            print(question)
-            print('1 %s'% answers[0])
-            print('2 %s'% answers[1])
-            print('3 %s'% answers[2])
             #只搜题目
-            start_browser_and_search(question)
-
-            #搜题目+选项
-            # start_browser_and_search(search_wd)
-
-            # print(question)
-            # met1 = metric1Func(question, answers)
-            # met2 = metric2Func(question, answers)
-            # return predict(met1, met2, answers)
+            start_browser_and_search(question, answers)
 
         else:
             return 'Waiting for new question...'
 
-def start_browser_and_search(search_wd):
-    print(search_wd)
-    s_url = 'https://www.baidu.com/s?wd=' + search_wd
-    search_question = urllib.parse.quote(search_wd)
-    webbrowser.open('https://www.baidu.com/s?wd=' + search_question)
-    input('按任意键继续')
+# def start_browser_and_search(search_wd):
+#     print(search_wd)
+#     s_url = 'https://www.baidu.com/s?wd=' + search_wd
+#     search_question = urllib.parse.quote(search_wd)
+#     webbrowser.open('https://www.baidu.com/s?wd=' + search_question)
+#     input('按任意键继续')
 
+def start_browser_and_search(question, answers):
+    m1 = Thread(methods.run_algorithm(0, question, answers))
+    m2 = Thread(methods.run_algorithm(1, question, answers))
+    m3 = Thread(methods.run_algorithm(2, question, answers))
+    m1.start()
+    m2.start()
+    m3.start()
+    input('暂停,按任意键继续')
 
 
 def main():
     while True:
         print(time.strftime('%H:%M:%S',time.localtime(time.time())))
-        # start_browser_and_search('二的六次方等于几')
+        # 测试问答
+        # start_browser_and_search(' 参加第一届古代奥运会的国家有', ['三个', '四个', '五个'])
+        # start_browser_and_search('以下哪个不是清华大学的代表校花', ['紫荆', '山茶花', '丁香'])
         get_answer()
         time.sleep(1)
 
---
+++ methods.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : Skye
+# @Time    : 2018/1/9 10:39
+# @desc    :
+
+import requests
+import webbrowser
+import urllib.parse
+
+def open_webbrowser(question):
+    webbrowser.open('https://baidu.com/s?wd=' + urllib.parse.quote(question))
+
+def open_webbrowser_count(question,choices):
+    print('\n-- 方法2： 题目+选项搜索结果计数法 --\n')
+    print('Question: ' + question)
+    if '不是' in question:
+        print('**请注意此题为否定题,选计数最少的**')
+
+    counts = []
+    for i in range(len(choices)):
+        # 请求
+        req = requests.get(url='http://www.baidu.com/s', params={'wd': question + choices[i]})
+        content = req.text
+        index = content.find('百度为您找到相关结果约') + 11
+        content = content[index:]
+        index = content.find('个')
+        count = content[:index].replace(',', '')
+        counts.append(count)
+        #print(choices[i] + " : " + count)
+    output(choices, counts)
+
+def count_base(question,choices):
+    print('\n-- 方法3： 题目搜索结果包含选项词频计数法 --\n')
+    # 请求
+    req = requests.get(url='http://www.baidu.com/s', params={'wd':question})
+    content = req.text
+    #print(content)
+    counts = []
+    print('Question: '+question)
+    if '不是' in question:
+        print('**请注意此题为否定题,选计数最少的**')
+    for i in range(len(choices)):
+        counts.append(content.count(choices[i]))
+        #print(choices[i] + " : " + str(counts[i]))
+    output(choices, counts)
+
+def output(choices, counts):
+    counts = list(map(int, counts))
+    #print(choices, counts)
+
+    # 最可能的答案
+    index_max = counts.index(max(counts))
+
+    # 最不可能的答案
+    index_min = counts.index(min(counts))
+
+    if index_max == index_min:
+        print("\033[1;31m此方法失效！\033[0m")
+        return
+
+    for i in range(len(choices)):
+        if i == index_max:
+            # 绿色为计数最高的答案
+            print("\033[1;32m{0:^10} {1:^10}\033[0m".format(choices[i], counts[i]))
+        elif i == index_min:
+            # 红色为计数最低的答案
+            print("\033[0;31m{0:^10}{1:^10}\033[0m".format(choices[i], counts[i]))
+        else:
+            print("{0:^10} {1:^10}".format(choices[i], counts[i]))
+
+
+def run_algorithm(al_num, question, choices):
+    if al_num == 0:
+        open_webbrowser(question)
+    elif al_num == 1:
+        open_webbrowser_count(question, choices)
+    elif al_num == 2:
+        count_base(question, choices)
+
+if __name__ == '__main__':
+    question = '新装修的房子通常哪种化学物质含量会比较高?'
+    choices = ['甲醛', '苯', '甲醇']
+    run_algorithm(1, question, choices)
+
+
